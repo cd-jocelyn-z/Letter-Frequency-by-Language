8678f9879bc10d551130a5d6f82a77836f6df2de
Jocelyn Zaruma <jocelyn.zaruma@proton.me>
2022-12-11 20:14:10 +0100

    modification in line spacing

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 # Creates dictionary of letters and calculates frequency of appearance in text.
 def get_letter_frequence(text):
-   
     text = ''.join(text.split())
     total_char = len(text)
 
@@ -21,13 +20,13 @@ get_letter_frequence('hey bob')
 
 # Retrieves all the paths to a txt file in corpus and adds them to a list.
 def get_files_in_coprus():
-
     list_of_paths = []
 
     for filename in os.listdir("corpus"):
         if filename.endswith(".txt"):
             file_path = os.path.join(os.getcwd(), "corpus", filename)
             list_of_paths.append(file_path)
+
     print(list_of_paths)
 
 
