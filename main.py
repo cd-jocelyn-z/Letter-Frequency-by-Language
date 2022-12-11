d852d9d5961740b70b47b3a977d9d4055c09cef2
Jocelyn Zaruma <jocelyn.zaruma@proton.me>
2022-12-11 20:04:11 +0100

    added new function and improved structure

--- main.py
+++ main.py
@@ -1,26 +1,34 @@
+import os
+
+# Creates dictionary of letters and calculates frequency of appearance in text.
 def get_letter_frequence(text):
-    # step 1: remove spaces
+   
     text = ''.join(text.split())
-    print(text)
-
-    # step 2: create dictionary
     my_dict = {key: 0 for key in text}
+    total_char = len(text)
 
-    # step 3: count the frequency of each letter in text, then increment its value in the dictionary
     for element in text:
         for key in my_dict:
             if element == key:
                 my_dict[key] += 1
-    #print(my_dict)
 
-    # step 4: retrieve how many charactes are in the text
-    total_char = len(text)
-    #print(total_char)
-
-    # step 5: calculate the percentage of frequency of each letter :
-    # using update() and item() methods with dictionary
     my_dict.update((key, float(value) / float(total_char) * 100) for key, value in my_dict.items())
     print(my_dict)
 
 
 get_letter_frequence('hey bob')
+
+
+# Retrieves all the paths to a txt file in corpus and adds them to a list.
+def get_files_in_coprus():
+
+    list_of_paths = []
+
+    for filename in os.listdir("corpus"):
+        if filename.endswith(".txt"):
+            file_path = os.path.join(os.getcwd(), "corpus", filename)
+            list_of_paths.append(file_path)
+    print(list_of_paths)
+
+
+get_files_in_coprus()
