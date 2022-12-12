425dcf30372342c9ae58c08d94a7c479e1dbf3c5
Jocelyn Zaruma <jocelyn.zaruma@proton.me>
2022-12-12 15:37:26 +0100

    modified previous and added components

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
+# main
 import os
-# Creates dictionary of letters and calculates frequency of appearance in text.
+# Retrieves letter frequence.
 def get_letter_frequence(text):
     text = ''.join(text.split())
+    text = text.lower()
     total_char = len(text)
 
-    my_dict = {key: 0 for key in text}
+    main_dict = {key: 0 for key in text}
 
     for element in text:
-        for key in my_dict:
+        for key in main_dict:
             if element == key:
-                my_dict[key] += 1
+                main_dict[key] += 1
 
-    my_dict.update((key, float(value) / float(total_char) * 100) for key, value in my_dict.items())
-    print(my_dict)
-
-
-get_letter_frequence('hey bob')
+    main_dict.update((key, float(value) / float(total_char) * 100) for key, value in main_dict.items())
+    # print("main dict: ", main_dict)
+    return main_dict
 
 
-# Retrieves all the paths to a txt file in corpus and adds them to a list.
+# Retrieves every path to every txt file in the folder corpus, organizes every path in a list.
 def get_files_in_coprus():
     list_of_paths = []
 
@@ -26,8 +26,31 @@ def get_files_in_coprus():
         if filename.endswith(".txt"):
             file_path = os.path.join(os.getcwd(), "corpus", filename)
             list_of_paths.append(file_path)
+    # print("list of paths: ", list_of_paths)
+    return list_of_paths
 
-    print(list_of_paths)
+
+# Retrieves vowel frequence.
+def get_vowel_frequence(main_dict):
+    vowel_list = ['a', 'e', 'i', 'o', 'u']
+    # print(vowel_list)
+
+    vowel_dict = {k: v for k, v in main_dict.items() if k in vowel_list}
+    # print("vowel dict: ", vowel_dict)
+    return vowel_dict
 
 
-get_files_in_coprus()
+# Retrieves vowel frequence of every language corpus.
+def get_corpus_vowel_frequence(list_of_paths):
+
+    for path in list_of_paths:
+        with open(path, "r") as file:
+            for document in file:
+                x = get_letter_frequence(document)
+                y = get_vowel_frequence(x)
+            print(path, "letter freq: ", x)
+            print(path, "vowel freq: ", y)
+
+
+paths_in_list = get_files_in_coprus()
+get_corpus_vowel_frequence(paths_in_list) 
