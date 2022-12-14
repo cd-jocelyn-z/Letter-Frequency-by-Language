84d927d76e8b7323f9fde09a5c2ebddd12cd85a0
Jocelyn Zaruma <jocelyn.zaruma@proton.me>
2022-12-14 13:07:21 +0100

    function

---
+++ frequencies_to_vector.py
@@ -0,0 +1,7 @@
+def frequencies_to_vector(lang_dict):
+
+
+
+
+lg_freqs = read_frequencies(’../corpus/frequences.csv’) f_v=frequencies_to_vector(lg_freqs[’en’])
+print(f_v) # affichera [8.167, 12.702, 6.966, 7.507, 2.758]
