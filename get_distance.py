a2de728015be5cf7a3469af38de3c016ce9a836d
Jocelyn Zaruma <jocelyn.zaruma@proton.me>
2022-12-14 13:10:06 +0100

    function

--- get_distance.py
+++ get_distance.py
@@ -1,17 +1,20 @@
+# Component 6 of main: distance to measure points between n-dimension
 import math
 
+# 2-dimension
 def dist_2d(p1,p2):
 
-    distance = (math.sqrt( ((p1[0]-p2[0])**2) + ((p1[1]-p2[1])**2) ))
-    return(distance)
+    distance = ((p1[0]-p2[0])**2) + ((p1[1]-p2[1])**2)
+
+    return math.sqrt(distance)
 
 print(dist_2d([4,0], [0,3]))
 print(dist_2d([5,2], [1,5]))
 
+# n-dimension
 def dist(p1,p2):
-    result = 0
+    distance = 0
     for index in range(len(p1)):
-        print(index)
-        result += (p1[index] - p2[index])**2
-    return math.sqrt(result)
+        distance += (p1[index] - p2[index])**2
+    return math.sqrt(distance)
 print(dist([4, 0, 3, 6, 1], [0, 3, 0, 5, 0]))
